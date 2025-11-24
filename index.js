abf9a22121ff034ca7649e78c71059fed93dc665
olu <olutest19@gmail.com>
2025-11-24 12:27:08 -0600

    Testing commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello DevOps'))
+app.get('/', (req, res) => res.send('Hello DevOps, This is a CI/CD Pipeline!'))
 app.listen(3000, () => console.log('Server running on port 3000'))
